30d5eaab4f8bd9007743bee9b573534f37632113
Luis Antonio <luis.developer.ac@gmail.com>
2022-07-06 21:05:52 -0500

    install MapView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { MapView } from 'expo'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
